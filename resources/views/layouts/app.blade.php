7b9ed3068a18091f3b3eac6aee5816828bfb7834
iosky07 <yoskitanjung07@gmail.com>
2021-09-01 17:32:10 +0700

    mix fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -192,7 +192,7 @@
         @livewireScripts
         {{--@livewireAlertScripts--}}
 
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
 
         @isset($script)
