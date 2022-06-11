b60372f63cc1bef9a2f6654295bb2c8405c02135
Cahyadi Setia Phanatagama <phanatagama@gmail.com>
2022-06-11 15:44:42 +0700

    feat: refactor file kabinet.blade.php
    Signed-off-by: Cahyadi Setia Phanatagama <phanatagama@gmail.com>

--- resources/views/pages/landing2/menu/kabinet.blade.php
+++ resources/views/pages/landing2/menu/kabinet.blade.php
@@ -109,10 +109,6 @@
 
   <section id="visi_misi">
     <div class="container">
-    <!-- <header class="section-header">
-        <h2>VISI MISI</h2>
-        <p>Visi & Misi Kabinet Catra Sahitya</p>
-      </header> -->
       <div class="row mb-3 p-3">
         <div class="col-md-12 col-lg-4">
           <div class="content_visi_kabinet_title mb-3">
@@ -260,8 +256,6 @@
             <div class="member-img">
               <img src={{asset("storage/img/member/" . $member->thumbnail)}} class="img-fluid" alt="{{ $member->name }}">
               <div class="social">
-                <!-- <a href=""><i class="bi bi-twitter"></i></a>
-              <a href=""><i class="bi bi-facebook"></i></a> -->
                 <a href="{{ $member->instagram }}"><i class="bi bi-instagram"></i></a>
                 <a href="{{ $member->linkedin }}"><i class="bi bi-linkedin"></i></a>
               </div>
@@ -269,70 +263,10 @@
             <div class="member-info">
               <h4 class="text-truncate">{{ $member->name }}</h4>
               <span>{{ $member->position . ' ' . $member->division }}</span>
-              <!-- <p>{{ $member->division }}</p> -->
-              <!-- <p>Velit aut quia fugit et et. Dolorum ea voluptate vel tempore tenetur ipsa quae aut. Ipsum exercitationem iure minima enim corporis et voluptate.</p> -->
             </div>
           </div>
         </div>
         @endforeach
-        <!-- 
-      <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
-        <div class="member">
-          <div class="member-img">
-            <img src={{asset("landing2/assets/img/team/team-2.jpg")}} class="img-fluid" alt="">
-            <div class="social">
-              <a href=""><i class="bi bi-twitter"></i></a>
-              <a href=""><i class="bi bi-facebook"></i></a>
-              <a href=""><i class="bi bi-instagram"></i></a>
-              <a href=""><i class="bi bi-linkedin"></i></a>
-            </div>
-          </div>
-          <div class="member-info">
-            <h4>Sarah Jhonson</h4>
-            <span>Product Manager</span>
-            <p>Quo esse repellendus quia id. Est eum et accusantium pariatur fugit nihil minima suscipit corporis. Voluptate sed quas reiciendis animi neque sapiente.</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
-        <div class="member">
-          <div class="member-img">
-            <img src={{asset("landing2/assets/img/team/team-3.jpg")}} class="img-fluid" alt="">
-            <div class="social">
-              <a href=""><i class="bi bi-twitter"></i></a>
-              <a href=""><i class="bi bi-facebook"></i></a>
-              <a href=""><i class="bi bi-instagram"></i></a>
-              <a href=""><i class="bi bi-linkedin"></i></a>
-            </div>
-          </div>
-          <div class="member-info">
-            <h4>William Anderson</h4>
-            <span>CTO</span>
-            <p>Vero omnis enim consequatur. Voluptas consectetur unde qui molestiae deserunt. Voluptates enim aut architecto porro aspernatur molestiae modi.</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
-        <div class="member">
-          <div class="member-img">
-            <img src={{asset("landing2/assets/img/team/team-4.jpg")}} class="img-fluid" alt="">
-            <div class="social">
-              <a href=""><i class="bi bi-twitter"></i></a>
-              <a href=""><i class="bi bi-facebook"></i></a>
-              <a href=""><i class="bi bi-instagram"></i></a>
-              <a href=""><i class="bi bi-linkedin"></i></a>
-            </div>
-          </div>
-          <div class="member-info">
-            <h4>Amanda Jepson</h4>
-            <span>Accountant</span>
-            <p>Rerum voluptate non adipisci animi distinctio et deserunt amet voluptas. Quia aut aliquid doloremque ut possimus ipsum officia.</p>
-          </div>
-        </div>
-      </div> -->
-
       </div>
 
     </div>
