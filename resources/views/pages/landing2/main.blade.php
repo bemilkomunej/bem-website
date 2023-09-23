477c1ff8a381a948387911b066809cc942227953
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-09-23 08:51:14 +0700

    CHANGE COLOR

--- resources/views/pages/landing2/main.blade.php
+++ resources/views/pages/landing2/main.blade.php
@@ -6,28 +6,31 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>BEM FASILKOM UNEJ</title>
-    <meta content="BEM merupakan lembaga eksekutif tertinggi di Fakultas Ilmu Komputer Universitas Jember" name="description">
+    <meta content="BEM merupakan lembaga eksekutif tertinggi di Fakultas Ilmu Komputer Universitas Jember"
+        name="description">
 
     <meta content="Website Badan Eksekutif Mahasiswa Fakultas Ilmu Komputer Universitas Jember" name="keywords">
 
     <!-- Favicons -->
-    <!-- <link href={{asset("landing2/assets/img/icon/favicon.png")}} rel="icon"> -->
-    <link href={{asset("landing2/assets/img/icon/apple-icon.png")}} rel="icon">
-    <link href={{asset("landing2/assets/img/icon/apple-touch-icon.png")}} rel="apple-touch-icon">
+    <!-- <link href={{ asset('landing2/assets/img/icon/favicon.png') }} rel="icon"> -->
+    <link href={{ asset('landing2/assets/img/icon/apple-icon.png') }} rel="icon">
+    <link href={{ asset('landing2/assets/img/icon/apple-touch-icon.png') }} rel="apple-touch-icon">
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href={{asset("landing2/assets/vendor/aos/aos.css")}} rel="stylesheet">
-    <link href={{asset("landing2/assets/vendor/bootstrap/css/bootstrap.min.css")}} rel="stylesheet">
-    <link href={{asset("landing2/assets/vendor/bootstrap-icons/bootstrap-icons.css")}} rel="stylesheet">
-    <link href={{asset("landing2/assets/vendor/glightbox/css/glightbox.min.css")}} rel="stylesheet">
-    <link href={{asset("landing2/assets/vendor/remixicon/remixicon.css")}} rel="stylesheet">
-    <link href={{asset("landing2/assets/vendor/swiper/swiper-bundle.min.css")}} rel="stylesheet">
+    <link href={{ asset('landing2/assets/vendor/aos/aos.css') }} rel="stylesheet">
+    <link href={{ asset('landing2/assets/vendor/bootstrap/css/bootstrap.min.css') }} rel="stylesheet">
+    <link href={{ asset('landing2/assets/vendor/bootstrap-icons/bootstrap-icons.css') }} rel="stylesheet">
+    <link href={{ asset('landing2/assets/vendor/glightbox/css/glightbox.min.css') }} rel="stylesheet">
+    <link href={{ asset('landing2/assets/vendor/remixicon/remixicon.css') }} rel="stylesheet">
+    <link href={{ asset('landing2/assets/vendor/swiper/swiper-bundle.min.css') }} rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href={{asset("landing2/assets/css/style.css")}} rel="stylesheet">
+    <link href={{ asset('landing2/assets/css/style.css') }} rel="stylesheet">
 
     <!-- =======================================================
   * Template Name: FlexStart - v1.9.0
@@ -44,19 +47,21 @@
         <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
             <a href="/" class="logo d-flex align-items-center">
-                <img src={{asset('landing2/assets/img/icon/apple-icon.png')}} alt="Logo Bem">
-                <span>BEM FASILKOM</span>
+                <img src={{ asset('landing2/assets/img/icon/apple-icon.png') }} alt="Logo Bem">
+                <span>BEM FASILKOM UNEJ</span>
             </a>
 
             <nav id="navbar" class="navbar">
                 <ul>
                     <li><a href="{{ route('home') }}">Beranda</a></li>
                     <li><a href="{{ route('blog') }}">Berita</a></li>
-                    <li class="dropdown"><a href="#"><span>Tentang Kami</span> <i class="bi bi-chevron-down"></i></a>
+                    <li class="dropdown"><a href="#"><span>Tentang Kami</span> <i
+                                class="bi bi-chevron-down"></i></a>
                         <ul>
                             <li><a href="{{ route('profil') }}">Profil</a></li>
-                            <li><a href="{{ route('kabinet')}}">Kabinet</a></li>
-                            <li class="dropdown"><a href="#"><span>Divisi</span> <i class="bi bi-chevron-right"></i></a>
+                            <li><a href="{{ route('kabinet') }}">Kabinet</a></li>
+                            <li class="dropdown"><a href="#"><span>Divisi</span> <i
+                                        class="bi bi-chevron-right"></i></a>
                                 <ul>
                                     <li><a href="{{ route('kominfo') }}">Kominfo</a></li>
                                     <li><a href="{{ route('perekonomian') }}">Perekonomian</a></li>
@@ -90,7 +95,7 @@
                         </ul>
                     </li>
                     <li><a class="nav-link scrollto" href="{{ route('ppmb') }}">PPMB</a></li>
-                    <li><a class="getstarted scrollto" href="/register">Login</a></li>
+                    <li><a class="getstarted scrollto" href="/login">Login</a></li>
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
             </nav><!-- .navbar -->
@@ -124,15 +129,18 @@
                 <div class="row gy-4">
                     <div class="col-lg-5 col-md-12 footer-info">
                         <a href="/" class="logo d-flex align-items-center">
-                            <img src={{asset('landing2/assets/img/icon/apple-icon.png')}} alt="Logo Bem">
+                            <img src={{ asset('landing2/assets/img/icon/apple-icon.png') }} alt="Logo Bem">
                             <span>BEMILKOMUNEJ</span>
                         </a>
                         <p>Badan Eksekutif Mahasiswa Fakultas Ilmu Komputer Universitas Jember.</p>
                         <div class="social-links mt-3">
                             <a href="https://t.me/bemilkomunej" class="telegram"><i class="bi bi-telegram"></i></a>
-                            <a href="https://web.facebook.com/bemilkomunej/" class="facebook"><i class="bi bi-facebook"></i></a>
-                            <a href="https://instagram.com/bemilkomunej" class="instagram"><i class="bi bi-instagram"></i></a>
-                            <a href="https://www.linkedin.com/company/bemilkom/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                            <a href="https://web.facebook.com/bemilkomunej/" class="facebook"><i
+                                    class="bi bi-facebook"></i></a>
+                            <a href="https://instagram.com/bemilkomunej" class="instagram"><i
+                                    class="bi bi-instagram"></i></a>
+                            <a href="https://www.linkedin.com/company/bemilkom/" class="linkedin"><i
+                                    class="bi bi-linkedin"></i></a>
                         </div>
                     </div>
 
@@ -181,21 +189,22 @@
         </div>
     </footer><!-- End Footer -->
 
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src={{asset("landing2/assets/vendor/purecounter/purecounter.js")}}></script>
-    <script src={{asset("landing2/assets/vendor/aos/aos.js")}}></script>
-    <script src={{asset("landing2/assets/vendor/bootstrap/js/bootstrap.bundle.min.js")}}></script>
-    <script src={{asset("landing2/assets/vendor/glightbox/js/glightbox.min.js")}}></script>
-    <script src={{asset("landing2/assets/vendor/isotope-layout/isotope.pkgd.min.js")}}></script>
-    <script src={{asset("landing2/assets/vendor/swiper/swiper-bundle.min.js")}}></script>
-    <script src={{asset("landing2/assets/vendor/php-email-form/validate.js")}}></script>
+    <script src={{ asset('landing2/assets/vendor/purecounter/purecounter.js') }}></script>
+    <script src={{ asset('landing2/assets/vendor/aos/aos.js') }}></script>
+    <script src={{ asset('landing2/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}></script>
+    <script src={{ asset('landing2/assets/vendor/glightbox/js/glightbox.min.js') }}></script>
+    <script src={{ asset('landing2/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}></script>
+    <script src={{ asset('landing2/assets/vendor/swiper/swiper-bundle.min.js') }}></script>
+    <script src={{ asset('landing2/assets/vendor/php-email-form/validate.js') }}></script>
 
     @yield('customscript')
 
     <!-- Template Main JS File -->
-    <script src={{asset("landing2/assets/js/main.js")}}></script>
+    <script src={{ asset('landing2/assets/js/main.js') }}></script>
 
     @livewireScripts
 </body>
