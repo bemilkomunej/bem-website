3994369a71f3f88e8c4f228a276c8d6e7d025e99
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-12-09 21:48:17 +0700

    Update speakerdeck link in profil.blade.php

--- resources/views/pages/landing2/menu/profil.blade.php
+++ resources/views/pages/landing2/menu/profil.blade.php
@@ -214,8 +214,9 @@
                         <!-- <iframe src="https://drive.google.com/file/d/1qhJKxj3hulmc6Xgewkh37WXylkdvUcU0/preview" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen" style="border-radius: 20px; width: 100%; height:100%;"></iframe> -->
                         <!-- widt 560 x 314 -->
                         <iframe class="speakerdeck-iframe" frameborder="0"
-                            src="https://speakerdeck.com/player/326090e1c47343d9b38f22fb736d9624"
-                            title="Profil BEM FASILKOM UNEJ 2022" allowfullscreen="true" mozallowfullscreen="true"
+                        {{-- src="https://speakerdeck.com/player/326090e1c47343d9b38f22fb736d9624" --}}
+                        src="https://speakerdeck.com/sibeux/profile-bem-fasilkom-unej-2023"
+                        title="Profil BEM FASILKOM UNEJ 2023" allowfullscreen="true" mozallowfullscreen="true"
                             webkitallowfullscreen="true"
                             style="border: 0px; background: padding-box padding-box rgba(0, 0, 0, 0.1); margin: 0px; padding: 0px; border-radius: 6px; box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 40px; width: 100%; height: 100%; min-height: 314px"
                             data-ratio="1.78343949044586"></iframe>
